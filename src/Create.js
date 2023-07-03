9a33f77c1c9b03f83bee5e33a4a668c4bd3d57fd
Yash <www.yashkunwar01@gmail.com>
2023-07-03 12:32:43 +0530

    update server

--- src/Create.js
+++ src/Create.js
@@ -9,7 +9,7 @@ const Create = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    fetch('http://localhost:8000/blogs/', {
+    fetch('https://jsonblog.onrender.com/', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
